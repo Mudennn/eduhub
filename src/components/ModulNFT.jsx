dfa1d90e9fb4ab02ffa5c2f83b70281ea2adc7a4
Muden <mudennn@yahoo.com>
2023-01-30 16:29:10 +0800

    Update module

--- src/components/ModulNFT.jsx
+++ src/components/ModulNFT.jsx
@@ -5,20 +5,25 @@ const ModulNFT = () => {
   return (
     <div className="text-white mx-auto m:max-w-xl md:max-w-full lg:max-w-screen-xl flex">
       <Sidebar />
-      <div className="grid w-full">
+      <div className="mx-auto w-full">
+        <div>
         <h2 className="text-2xl mt-5 ml-5 font-semibold">1.1 Introduction</h2>
-        <div className="mt-10 mx-auto ">
+        </div>
+        <div className="flex items-center justify-center mt-10">
           <div className="aspect-w-16 aspect-h-9">
             <iframe
               width="560"
               height="315"
               src="https://www.youtube.com/embed/zEA3cbJI8io?start=962"
-              title="YouTube video player"
+              title="Digital Wallet"
               frameborder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              allowfullscreen
+              allowFullScreen
             ></iframe>
           </div>
+        </div>
+        <div className="mx-auto px-20 mt-10">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium ut dicta in, ducimus quidem voluptatem culpa alias soluta, maxime dolorem laudantium fuga modi reiciendis, debitis sint voluptatum nihil excepturi sapiente?</p>
         </div>
       </div>
     </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Sidebar = () => {
   return (
     <div>
-      <aside className="w-full p-6 sm:w-60 bg-gray-900 text-gray-100 ">
+      <aside className="w-full p-6 sm:w-60 bg-gray-900 text-gray-100 min-h-screen">
         <nav className="space-y-8 text-sm">
           <div className="space-y-2">
             <h2 className="text-sm font-semibold tracking-widest uppercase dark:text-gray-400">
